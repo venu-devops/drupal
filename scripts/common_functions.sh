bc3784734f4bc23c2a1b9b6f1a0ec59a3d355f80
venu-devops <venu_rama@harvard.edu>
2015-08-12 13:12:03 -0400

    Updated ELB name and S3 bucket

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -14,7 +14,7 @@
 # permissions and limitations under the License.
 
 # ELB_LIST defines which Elastic Load Balancers this instance should be part of.
-ELB_LIST=""
+ELB_LIST="devops-bootcam-Elb-NA4NH89P3ZXL"
 
 # Under normal circumstances, you shouldn't need to change anything below this line.
 # -----------------------------------------------------------------------------
